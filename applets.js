ea74e1b9a59e1006d257b04b73b5847a04bac73b
ralph2023o <ralphhope7@gmail.com>
2024-10-18 22:25:34 +0800

    Created a create card function

--- applets.js
+++ applets.js
@@ -5,3 +5,26 @@ class AppletCard {
         this.description = description;
         this.link = link;
     }
+    createCard() {
+        const cardDiv = document.createElement('div');
+        cardDiv.className = 'card applet-card';
+        cardDiv.innerHTML = `
+            <div class="card"  data-bs-toggle="tooltip" title="${this.description}">
+                <a href="${this.link}" style="text-decoration: none; color: inherit;">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-4">
+                                <img src="${this.icon}" class="applet-icon rounded float-start">
+                            </div>
+                            <div class="col-8">
+                                <h5 class="card-title">${this.title}</h5>
+                                <p class="card-text"</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        `;
+        return cardDiv;
+    }
+}
