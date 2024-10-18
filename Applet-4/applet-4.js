d021a3f3f0d8dd10e9cb4e4e8bbcdd84b5642e1c
ralph2023o <ralphhope7@gmail.com>
2024-10-18 18:08:59 +0800

    Created an array of student objects

--- Applet-4/applet-4.js
+++ Applet-4/applet-4.js
@@ -12,4 +12,15 @@ class StudentList {
             console.error('Error fetching data:', error);
         }
     }
-    
+    renderStudentList(students) {
+        const studentSearchListContainer = document.getElementById('studentSearchList');
+        studentSearchListContainer.innerHTML = ''; 
+
+        students.forEach(student => {
+            studentSearchListContainer.innerHTML += `
+                <p> ${student.student_name}</p>
+                <p class="fw-light"> ${student.student_program} </p>
+                <hr>
+            `;
+        });
+    }
