ec087ce9d0ad4a644a77f7b8afac06cee7058cc9
ralph2023o <ralphhope7@gmail.com>
2024-10-18 22:24:34 +0800

    Created a appletcard class

---
+++ applets.js
@@ -0,0 +1,7 @@
+class AppletCard {
+    constructor(title,icon, description, link) {
+        this.title = title;
+        this.icon = icon;
+        this.description = description;
+        this.link = link;
+    }
