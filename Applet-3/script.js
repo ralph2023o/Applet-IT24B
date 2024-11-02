cfc014c4a55c350b29a9de752f85e5b685a6d05b
ralph2023o <ralphhope7@gmail.com>
2024-11-02 11:03:52 +0800

    Added the method that updates the card container

--- Applet-3/script.js
+++ Applet-3/script.js
@@ -57,3 +57,20 @@ class DataLogger {
         this.checkedInUsers = []; // Clear checked-in array
         this.updateCardContainer();
     }
+    updateCardContainer() {
+        this.cardContainer.innerHTML = '';
+
+        this.loggedData.forEach(data => {
+            const card = document.createElement('div');
+            card.className = 'card mb-2';
+            card.innerHTML = `
+                <div class="card-body">
+                    <h5 class="card-title">Logged Data</h5>
+                    <p class="card-text">${data}</p>
+                </div>
+            `;
+            this.cardContainer.appendChild(card);
+        });
+
+        this.displayLogCount();
+    }
