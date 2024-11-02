897c902e04aa8365a7b62a2593718f2adae6cdd6
ralph2023o <ralphhope7@gmail.com>
2024-11-02 11:00:45 +0800

    Added a logdata method

--- Applet-3/script.js
+++ Applet-3/script.js
@@ -13,3 +13,22 @@ class DataLogger {
         this.clearButton.addEventListener('click', () => this.clearLogs());
         this.outButton.addEventListener('click', () => this.OutData());
     }
+    logData() {
+        const name = this.nameInput.value.trim();
+
+        if (!name) {
+            alert('Please enter your name.');
+            return;
+        }
+        if (this.checkedInUsers.includes(name)) {
+            alert(`${name} is already checked in. Please check out first.`);
+            return;
+        }
+
+        const timestamp = new Date().toLocaleString();
+        this.loggedData.push(`${name} Check In: ${timestamp}`);
+        this.checkedInUsers.push(name); 
+        this.updateCardContainer();
+        console.log(`${name} checked in`);
+        this.nameInput.value = '';
+    }
